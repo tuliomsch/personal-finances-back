ac8f1aa55f20bf08947d9cab853d64f412165dd3
tuliomsch <crmunoz4@uc.cl>
2026-01-26 00:31:20 -0300

    feat: se agregan funcionalidades de crear, actualizar y borrar categorías

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,24 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsNumber, IsString, IsOptional } from 'class-validator';
+import { CategoryType } from 'src/generated/prisma/enums';
+
+export class CreateCategoryDto {
+    @IsNotEmpty()
+    @IsNumber()
+    userId: number;
+
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    icon: string;
+
+    @IsNotEmpty()
+    @IsString()
+    type: CategoryType;
+
+    @IsNumber()
+    @IsOptional()
+    parentId?: number;
+}
